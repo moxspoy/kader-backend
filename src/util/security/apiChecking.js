de59ef41e8b04d2157cb58eedd2d446549167bf4
moxspoy <mnurilmanbaehaqi@gmail.com>
2020-05-21 17:26:01 +0700

    simplify ap checking syntax

--- src/util/security/apiChecking.js
+++ src/util/security/apiChecking.js
@@ -1,12 +1,12 @@
 const {API_KEY_ALLOWED} = require('../../constants/apiConstant');
-const {error} = require('../../error/errorHandler');
+const {generateErrorResponse} = require('../../error/errorHandler');
+const {INVALID_API_KEY} = require('../../constants/errorCodes');
 
 const checkingApiKeyHandler = (req, res, next) => {
     const key = req.headers['api-key'];
-    if (!key) return res.send(error(400, 'API key required. Contact administrator to obtain API Key.'));
-    if (!~API_KEY_ALLOWED.indexOf(key)) return res.send(error(401, 'Invalid API Key.'));
+    const isInvalid = !key || !~API_KEY_ALLOWED.indexOf(key);
+    if (isInvalid) return res.send(generateErrorResponse(401, INVALID_API_KEY, 'API key required. Contact administrator to obtain API Key.'));
     req.key = key;
-    console.log('req_key', req.key);
     next();
 }
 
