c0478e0805b0c3bfb52afb8734e43bc044e77f23
moxspoy <mnurilmanbaehaqi@gmail.com>
2020-05-23 18:34:51 +0700

    remove session and use implement function jwt

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
-const jwt = require('express-jwt');
+
 const {checkingApiKeyHandler} = require('./src/util/security/apiChecking');
 const {sessionManagement} = require('./src/util/security/sessionManagement');
 const {generateErrorResponse} = require('./src/error/errorHandler');
 const {JWT_SECRET} = require('./src/constants/securityConstant');
+const {jwt} = require('./src/util/security/jwt');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -25,25 +26,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored
-  secret: 'shhhh, very secret'
-}));
-
-app.use(function (req, res, next) {
-  const err = req.session.error;
-  const msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = '';
-  if (err) res.locals.message = console.log(err);
-  if (msg) res.locals.message = console.log(msg);
-  next();
-});
-
-// app.use(jwt({secret: JWT_SECRET}).unless({path: ['/', '/users/login',]}));
-app.use('/', sessionManagement);
+app.use(jwt());
 app.use('/', checkingApiKeyHandler);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
